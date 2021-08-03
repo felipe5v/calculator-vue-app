d7c746db731eeeb15d8cb8240cf8c43fa0734b8d
felipe5v <luisfelipe432@hotmail.com>
2021-08-03 15:08:59 -0500

    calculator v1.0

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div
+    class="absolute left-0 right-0 bg-skin-base flex flex-col items-center justify-center h-max min-h-full overflow-x-auto"
+    :class="`theme-${theme}`"
+  >
+    <div class="my-auto md:min-h-4/5 md:w-2/5 w-4/5 min-h-9/10 relative p-6">
+      <header-calc />
+      <screen-calc />
+      <keys-calc />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { reactive, ref } from "@vue/reactivity";
+  import HeaderCalc from "./components/HeaderCalc.vue";
+  import ScreenCalc from "./components/ScreenCalc.vue";
+  import KeysCalc from "./components/KeysCalc.vue";
+  import { provide } from "@vue/runtime-core";
+  export default {
+    components: { HeaderCalc, ScreenCalc, KeysCalc },
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    setup() {
+      const theme = ref(1);
+      provide("theme", theme);
+      const screen = reactive([]);
+      provide("screen", screen);
+      return { theme };
+    },
+  };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Key.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="btn" :class="className">
+    {{ data }}
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      data: String,
+      "class-name": {
+        type: String,
+        default: "btn-base",
+      },
+      value: String,
+    },
+  };
+</script>
+
+<style></style>
